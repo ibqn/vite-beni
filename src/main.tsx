a1172c41f395918654768008af180c0b61f33ef9
ibqn <evgen.ibqn@gmail.com>
2023-09-17 17:45:54 +0200

    update main

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { App } from '@/app'
 import '@/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const element = document.getElementById('root')
+
+if (element) {
+  ReactDOM.createRoot(element).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+} else {
+  throw new Error('no root element found')
+}
